07d1f44a74671b5af8bb9c12b54b0271609938ca
nicolaslrod <rodriguez.n@outlook.com>
2020-12-15 20:18:28 -0300

    Agregamos h2 console y ajustes a front y modelos

--- grails-app/domain/epers/tactics/Aventurero.groovy
+++ grails-app/domain/epers/tactics/Aventurero.groovy
@@ -8,6 +8,7 @@ class Aventurero {
     static mapping = {
     id(generator: 'org.hibernate.id.enhanced.SequenceStyleGenerator', 
         params: [sequence_name: 'start_seq', initial_value: 1])
+    version false
     }
 
     static constraints = {
--- grails-app/domain/epers/tactics/Party.groovy
+++ grails-app/domain/epers/tactics/Party.groovy
@@ -9,6 +9,7 @@ class Party {
     static mapping = {
     id(generator: 'org.hibernate.id.enhanced.SequenceStyleGenerator', 
         params: [sequence_name: 'start_seq', initial_value: 1])
+     version false
     }
 
     static constraints = {
